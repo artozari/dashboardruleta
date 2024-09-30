00d07b4e0ec8d6dbbea1d8e5fa584c849bd23737
Angel Artozari <artozaria@sielcon.com.ar>
2024-09-30 16:50:37 -0300

    viendo como hacer para que se guarden los datos correctamente y se eliminen los mas antiguos en el objeto<record> Mesas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,20 +20,40 @@ import { ConectorComponent, Mesa } from './conector/conector.component';
 })
 
 export class AppComponent {
+
   conexion : ConectorComponent;
   dato: Mesa = {} as Mesa;
+  Mesas: Record<string, Mesa> = {};
+  i:number=0;
 
   constructor() {
+
     this.conexion = new ConectorComponent();
     this.conexion.conectar();
-
-
+    
     this.conexion.client.subscribe(this.conexion.topico);
+    
     this.conexion.client.on('message', (topic:string, message:Uint8Array) => {
-      this.dato = JSON.parse(message.toString());
-      console.log(this.dato.gameNumber);
+      this.dato = JSON.parse(message.toString()); 
+      // console.log(this.dato);
+      
+      this.Mesas[this.i] ={
+        ts: this.dato.ts,
+        gameNumber: this.dato.gameNumber,
+        casinoData: this.dato.casinoData,
+        tableData: this.dato.tableData,
+        configData: this.dato.configData,
+        winningNumbersData: this.dato.winningNumbersData
+      }
+      console.log(this.i);
+      // console.log(this.Mesas[this.i].gameNumber);
+      console.log(this.Mesas[this.i].gameNumber);
+      
+      this.i++;
+      this.i >= 5 && delete this.Mesas[this.i-5];
+      console.log(this.Mesas);
+      
     });
-
-  
   }
+
 }
--- src/app/conector/conector.component.ts
+++ src/app/conector/conector.component.ts
@@ -32,11 +32,9 @@ export class ConectorComponent {
   client: any;
 
   //# Variables de IU
-  // data: string = "";
-  // numMesas: number;
   
   constructor() {    
-    this.cantMesas = 100;
+    this.cantMesas = 50;
     this.server = "ws://sielcondev01.site:9105";
     this.clientName = "mqttClinte";
     this.topico = "sts/dashboard/local/CA_SLCN/#";
@@ -47,52 +45,5 @@ export class ConectorComponent {
       clientId: this.clientName,
       clean: true,
     });
-
-    this.client.on('connect', () => {
-      this.client.subscribe(this.topico);
-    });
-
-    this.client.on("message", (topic:string, message:string) => {
-      // console.log(message.toString());
-      console.log("mensaje recibido");
-      // this.client.end();
-    });
   }
-
-  getPublicaciones(): string {
-    let messageReceived: string = '';
-  
-    this.client.on("message", (topic: string, message: string) => {
-      messageReceived = message.toString();
-    });
-  
-    return messageReceived;
-  }
-
-  // publish(topic: string, message: string) {
-  //   this.client.publish(topic, message);
-  //   //# Aqui se pubica el json modiicado con los datos random cada X tiempo
-  //   for(let i = 1; i <= this.cantMesas; i++){
-  //     setInterval(() => {
-  //       this.client.publish(`${this.topico}${(i)}`, `${JSON.stringify(this.modJson(jsondata))}`);
-  //       console.log(`${this.topico}`, `${JSON.stringify(this.modJson(jsondata))}`);
-  //     }, 10000);
-  //   }
-  // }
-  
-  // modJson(datajson:Mesa) {
-  //   let nuevoDataJason = {
-  //     ...datajson, 
-  //     ts:5,
-  //     gameNumber: Math.floor(Math.random() * 37),
-  //     configData: this.seleccionarAleatorio()
-  //   };
-  //   return nuevoDataJason
-  // }
-
-  // seleccionarAleatorio(){
-  //   const valores = ['fr37', 'fr38', 'am38'];
-  //   const indiceAleatorio = Math.floor(Math.random() * valores.length);
-  //   return valores[indiceAleatorio];
-  // }
 }  
