5318fb4ad9258a69906f9af0e5288611a6e8d2ce
unknown <artozari@gmail.com>
2024-10-02 09:23:35 -0300

    mostrando estadistica en barra inestable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,39 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-
-//* locales
 import { BodyMainComponent } from "./body-main/body-main.component";
 import { FooterComponent } from './footer/footer.component';
 import { CabeceraComponent } from "./cabecera/cabecera.component";
-
-//* utiles
 import { ConectorComponent, Mesa } from './conector/conector.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ConectorComponent, RouterOutlet, CabeceraComponent , MatButtonModule, BodyMainComponent, FooterComponent, BodyMainComponent , MatMenuModule],
+  imports: [ConectorComponent, RouterOutlet, CabeceraComponent , MatButtonModule, BodyMainComponent, FooterComponent, BodyMainComponent , MatMenuModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
 
+  formSelectTable = new FormGroup({
+    mesaInput: new FormControl(''),
+  });
+  
+  mostrarMesa = false;
+  inpMesa = document.getElementById("mesaInput");
   conexion : ConectorComponent;
   dato: Mesa = {} as Mesa;
   Mesas: Record<string, Mesa> = {};
   i:number=0;
+  mesaArray:string[];
+
+  
 
   constructor() {
 
+    this.mesaArray = [];
     this.conexion = new ConectorComponent();
     this.conexion.conectar();
     
@@ -35,8 +42,6 @@ export class AppComponent {
     
     this.conexion.client.on('message', (topic:string, message:Uint8Array) => {
       this.dato = JSON.parse(message.toString()); 
-      // console.log(this.dato);
-      
       this.Mesas[this.i] ={
         key: this.i,
         ts: this.dato.ts,
@@ -46,9 +51,35 @@ export class AppComponent {
         configData: this.dato.configData,
         winningNumbersData: this.dato.winningNumbersData
       }
-      console.log(this.Mesas);  
+      // console.log(this.Mesas);
       this.i++;
     });
   }
+  cambiarMostrarMesa(){
+    this.mostrarMesa = !this.mostrarMesa;
+    if (this.mostrarMesa) {
+      const valMesaInput = this.formSelectTable.get('mesaInput');
+      if (valMesaInput) {
+        let val = valMesaInput?.value;
+        // console.log(val);
+        this.obtenerMesaDeMesas(val)
+      }else{
+        console.log("no hay datos");
+        
+      }
+      
+      }
+    }
+  
+
+  obtenerMesaDeMesas(keyMesa:any):void{
+    // console.log(this.Mesas[keyMesa]);
+    this.mesaArray[0] = this.Mesas[keyMesa].key.toString();
+    this.mesaArray[1] = this.Mesas[keyMesa].ts.toString();
+    this.mesaArray[2] = this.Mesas[keyMesa].gameNumber.toString();
+    console.log(this.Mesas);
+    
+  }
 
 }
+
--- src/app/body-main/body-main.component.ts
+++ src/app/body-main/body-main.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, Input } from '@angular/core';
+import * as echarts from "echarts";
 
 @Component({
   selector: 'app-body-main',
@@ -9,6 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './body-main.component.css'
 })
 export class BodyMainComponent {
+  
+  // mesaSolicitada:number = 0;
+  @Input() mesa:string[] = [];
+  
+  ngAfterViewInit(){
+    let myChart = echarts.init(document.getElementById('main'));
+  
+      // Specify the configuration items and data for the chart
+      let option = {
+        title: {
+          text: 'Ruleta Simulada Sielcon'
+        },
+        tooltip: {},
+        legend: {
+          data: ['MESAS']
+        },
+        xAxis: {
+          data: [this.mesa[2]]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: 'MESAS',
+            type: 'bar',
+            data: [this.mesa[2]]
+          }
+        ]
+      };
+      myChart.setOption(option);
+      //body-main.component.ts:40 MESAS series not exists. Legend data should be same with series name or data name.
+  }
 
 }
 
