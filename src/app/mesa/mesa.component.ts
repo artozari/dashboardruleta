17859d86bc6de9e8ab98e9aa2ef7cfb597dc16e2
Angel Artozari <artozaria@sielcon.com.ar>
2024-10-16 12:06:33 -0300

    mostrando varias mesas simultaneamente

--- src/app/mesa/mesa.component.ts
+++ src/app/mesa/mesa.component.ts
@@ -1,4 +1,4 @@
-import { Component,  Input,  Output,  SimpleChanges} from '@angular/core';
+import { Component,  Input,  Output, OnChanges,  SimpleChanges} from '@angular/core';
 import * as echarts from 'echarts';
 import { Mesa } from '../conector/conector.component';
 
@@ -10,44 +10,50 @@ import { Mesa } from '../conector/conector.component';
   styleUrl: `./mesa.component.css`
 })
 
-export class MesaComponent {
+export class MesaComponent implements OnChanges {
 
   @Input() dato = {} as Mesa;
   @Output() nroTable:number=1;
   miNewVar:string="para el commit"
   myChart:any;
-  option = {}
-  ngOnChanges(changes: SimpleChanges) {
+  option = {};
+  constructor(){
     
-    // if (this.myChart) {
-    //   this.myChart.remove();
-    // }
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data'] && changes['data'].currentValue) {
+      if (this.myChart) {
+        this.myChart.dispose();
+        let myChart:any;
+        this.myChart = echarts.init(document.getElementById(this.dato.tableData[1].toString())); 
+    }
+  }
     this.myChart = echarts.init(document.getElementById(this.dato.tableData[1].toString())); 
-      this.option = {
-        title: {
-          text: `${this.dato.tableData[3]}`
-            },
-            tooltip: {},
-            legend: {
-              data: ['Juego']
-            },
-            xAxis: {
-              data: [this.dato.winningNumbersData[0][2],this.dato.winningNumbersData[1][2],this.dato.winningNumbersData[2][2],this.dato.winningNumbersData[3][2],this.dato.winningNumbersData[4][2]]
-            },
-            yAxis: {},
-            series: [
-              {
-                name: 'Juego',
-                type: 'line',
-                smooth: false,
-                data: [this.dato.winningNumbersData[0][3],this.dato.winningNumbersData[1][3],this.dato.winningNumbersData[2][3],this.dato.winningNumbersData[3][3],this.dato.winningNumbersData[4][3]]
-              }
-            ]
-          };
-          
-          this.myChart.setOption(this.option);
-          // Realizar alguna acción con el nuevo valor
-        };
+    this.option = {
+      title: {
+        text: `${this.dato.tableData[3]}`
+      },
+      tooltip: {},
+      legend: {
+        data: ['Juego']
+      },
+      xAxis: {
+        data: [this.dato.winningNumbersData[0][2],this.dato.winningNumbersData[1][2],this.dato.winningNumbersData[2][2],this.dato.winningNumbersData[3][2],this.dato.winningNumbersData[4][2]]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'Juego',
+          type: 'line',
+          smooth: false,
+          data: [this.dato.winningNumbersData[0][3],this.dato.winningNumbersData[1][3],this.dato.winningNumbersData[2][3],this.dato.winningNumbersData[3][3],this.dato.winningNumbersData[4][3]]
+        }
+      ]
+    };
+    
+    this.myChart.setOption(this.option);
+    // Realizar alguna acción con el nuevo valor
+  };
 
         cambiarMostrarMesa(){
           this.dato.tableData[1];
